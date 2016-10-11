d053e4c0462c682c93b9b6c31574ed441a94bab6
nathanww <nathanww@gmail.com>
2016-10-11 18:41:19 -0500

    Added basic sleep mode--plays white noise and cues when device flipped over.

---
+++ app/src/main/java/edu/northwestern/langlearn/MainActivity.java
@@ -0,0 +1,25 @@
+package edu.northwestern.langlearn;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sleepButton = (Button) findViewById(R.id.sleep); //button to start sleep mode
+        sleepButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, sleepMode.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+    }
+}
