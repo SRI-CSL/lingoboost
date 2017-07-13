05c1d49fcd56cbddd591734829a8d6029fcfc570
John Pywtorak <jpywtora@gmail.com>
2017-07-13 00:55:49 -0700

    Working on handling the text input, kinda working in progress

--- app/src/main/kotlin/edu/northwestern/langlearn/TestActivity.kt
+++ app/src/main/kotlin/edu/northwestern/langlearn/TestActivity.kt
@@ -7,13 +7,32 @@ import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.KeyEvent
 import android.widget.EditText
 
 import org.jetbrains.anko.longToast
 import kotlinx.android.synthetic.main.activity_words.*
 import java.io.IOException
 import java.io.OutputStreamWriter
+
+//fun EditText.onTextChanged(onTextChanged: (CharSequence?, Int, Int, Int) -> Unit) {
+//    this.addTextChangedListener(object : TextWatcher {
+//        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//            onTextChanged.invoke(s, start, before, count)
+//        }
+//        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+//        override fun afterTextChanged(editable: Editable?) { }
+//    })
+//}
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+    })
+}
 
 class TestActivity : WordsProviderUpdate, AppCompatActivity() {
     override val wordsProviderUpdateActivity: AppCompatActivity
@@ -38,31 +57,51 @@ class TestActivity : WordsProviderUpdate, AppCompatActivity() {
         wordsProvider.fetchJSONWords(this)
 
         // words_edit_word.isFocusable = true
-        words_edit_word.setOnFocusChangeListener { v, hasFocus ->
-            Log.d(TAG, "hasFocus: $hasFocus")
-        }
-        words_edit_word.setOnKeyListener { v, keyCode, event ->
-            Log.d(TAG, "keyCode: $keyCode")
+        // words_edit_word.setOnFocusChangeListener { v, hasFocus ->
+        //     Log.d(TAG, "hasFocus: $hasFocus")
+        // }
 
-            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                Log.d(TAG, "Enter pressed")
-                logTestResults { continueWordTesting() }
-                true
-            } else {
-                false
+//        words_edit_word.onTextChanged { s, start, before, count -> Log.d(TAG, "Wow!") }
+        words_edit_word.afterTextChanged {
+            if (it.isNotEmpty()) {
+                if (it.last() == '\n') {
+                    words_edit_word.setText(it)
+                    words_edit_word.setSelection(it.length)
+                    logTestResults { continueWordTesting() }
+                }
             }
         }
 
-        words_edit_word.addTextChangedListener(object: TextWatcher {
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            }
-
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            }
-
-            override fun afterTextChanged(s: Editable) {
-            }
-        })
+//        words_edit_word.addTextChangedListener(object: TextWatcher {
+//            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+//                if ((start >= s.length) or (start < 0) or s.isEmpty())
+//                    return
+//
+//                if (s.subSequence(start, start + 1).last() == '\n') {// if (s.last() == '\n') {
+//                    val s1 = if (start > 0) s.subSequence(0, start).toString() else ""
+//                    val s2 = if (start < s.length) s.subSequence(start + 1, s.length).toString() else ""
+//                    val text = "$s1$s2"
+//
+//                    words_edit_word.setText(text)
+//                    words_edit_word.setSelection(text.length)
+//                    logTestResults { continueWordTesting() }
+//                }
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
+//            override fun afterTextChanged(s: Editable) {
+//                if (s.isEmpty())
+//                    return
+//
+//                if (s.last() == '\n') {
+//                    val text = s.toString()
+//
+//                    words_edit_word.setText(text)
+//                    words_edit_word.setSelection(text.length)
+//                    logTestResults { continueWordTesting() }
+//                }
+//            }
+//        })
     }
 
     override fun updateJSONWords(json: String) {
@@ -87,7 +126,6 @@ class TestActivity : WordsProviderUpdate, AppCompatActivity() {
             val word = words.get(wordsIndex).word
 
             words_text_word.text = word
-            longToast("Playing $word")
         }
     }
 
