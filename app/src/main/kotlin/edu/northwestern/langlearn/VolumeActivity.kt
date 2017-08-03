875bc4833069251c9d8632edf47835db695bfbdb
John Pywtorak <jpywtora@gmail.com>
2017-08-03 16:06:52 -0700

    Volume's and sliders in place for word volume and white noise with next through to test words. Still a work in progress.

--- app/src/main/kotlin/edu/northwestern/langlearn/VolumeActivity.kt
+++ app/src/main/kotlin/edu/northwestern/langlearn/VolumeActivity.kt
@@ -1,15 +1,26 @@
 package edu.northwestern.langlearn
 
+import android.content.Intent
+import android.graphics.PointF
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.SeekBar
 
 import java.io.IOException
 
 import kotlinx.android.synthetic.main.activity_volume.*
+import org.jetbrains.anko.contentView
+import android.os.SystemClock
+
+import com.agilie.volumecontrol.animation.controller.ControllerImpl
+import com.agilie.volumecontrol.getPointOnBorderLineOfCircle
+import com.agilie.volumecontrol.view.VolumeControlView.Companion.CONTROLLER_SPACE
 
 class VolumeActivity : AppCompatActivity() {
     private val TAG = javaClass.simpleName
@@ -19,7 +30,95 @@ class VolumeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_volume)
 
+        // words_volume.setBackgroundShiningColor(Color.rgb(1, 1, 1))
+        words_volume.setStartPercent(50)
+        seek_bar_words.progress = 50
+
+        // white_noise_volume.setBackgroundShiningColor(Color.WHITE)
+        // white_noise_volume.isEnabled = false
+        // white_noise_volume.isFocusable = false
+        white_noise_volume.setStartPercent(25)
+        seek_bar_white_noise.progress = 25
+
+
         val sP = PreferenceManager.getDefaultSharedPreferences(baseContext)
+
+
+
+
+        words_volume.controller?.onTouchControllerListener = (object : ControllerImpl.OnTouchControllerListener {
+            override fun onControllerDown(angle: Int, percent: Int) { }
+            override fun onControllerMove(angle: Int, percent: Int) { }
+            override fun onAngleChange(angle: Int, percent: Int) = seek_bar_words.setProgress(percent)
+        })
+        white_noise_volume.controller?.onTouchControllerListener = (object : ControllerImpl.OnTouchControllerListener {
+            override fun onControllerDown(angle: Int, percent: Int) { }
+            override fun onControllerMove(angle: Int, percent: Int) { }
+            override fun onAngleChange(angle: Int, percent: Int) = seek_bar_white_noise.setProgress(percent)
+        })
+        seek_bar_words.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // words_volume.setStartAngle((progress *  360) / 100)
+                // MotionEvent.obtain(downTime, eventTime, MotionEvent.ACTION_DOWN, x, y, 0)
+                // words_volume.onTouch(contentView!!, MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 0f, 0f, 0))
+
+                if (!fromUser) return
+
+                val w: Int = words_volume.width
+                val h: Int = words_volume.height
+                val controllerCenter = PointF().apply {
+                    x = w / 2f
+                    y = h / 2f
+                }
+                val controllerRadius = if (w > h) h / CONTROLLER_SPACE else w / CONTROLLER_SPACE
+                val restoreTouchPoint = getPointOnBorderLineOfCircle(controllerCenter, controllerRadius, (progress *  360) / 100)
+                // Obtain MotionEvent object
+                // val downTime = SystemClock.uptimeMillis()
+                // val eventTime = SystemClock.uptimeMillis() + 100
+                // List of meta states found here: developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
+                val metaState = 0
+                val motionEvent = MotionEvent.obtain(
+                        // downTime,
+                        // eventTime,
+                        0,
+                        0,
+                        MotionEvent.ACTION_DOWN,
+                        restoreTouchPoint.x,
+                        restoreTouchPoint.y,
+                        metaState
+                )
+
+                // Dispatch touch event to view
+                // contentView?.dispatchTouchEvent(motionEvent)
+                words_volume.onTouch(contentView!!, motionEvent)
+                // motionEvent.recycle()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) { }
+            override fun onStopTrackingTouch(seekBar: SeekBar) { }
+        })
+        volume_next.setOnClickListener(View.OnClickListener {
+            Log.d(TAG, "next OnClickListener")
+
+            if (words_volume.visibility == View.VISIBLE) {
+                words_volume.visibility = View.GONE
+                seek_bar_words.visibility = View.GONE
+                text_view_word_playback.visibility = View.GONE
+                white_noise_volume.visibility = View.VISIBLE
+                seek_bar_white_noise.visibility = View.VISIBLE
+                text_view_white_noise.visibility = View.VISIBLE
+            } else {
+                words_volume.visibility = View.VISIBLE
+                seek_bar_words.visibility = View.VISIBLE
+                text_view_word_playback.visibility = View.GONE
+                white_noise_volume.visibility = View.GONE
+                seek_bar_white_noise.visibility = View.GONE
+                text_view_white_noise.visibility = View.GONE
+
+                val tIntent: Intent = Intent(this, TestActivity::class.java)
+
+                startActivity(tIntent)
+            }
+        })
     }
 
     override fun onDestroy() {
